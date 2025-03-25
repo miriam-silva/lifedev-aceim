42698398ed2b414827ef6e118252425c4ee78637
isageorgete <isadorageorgete22@gmail.com>
2025-03-25 12:15:00 -0300

    aula 2503

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
 
 function App() {
   
   return (
     <>
       <Navbar />
+      <Footer/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,20 @@ import { useStat } from 'react'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
-    const [user, setUser] = useState({name:'Ana', idade:19})
+  const [user, setUser] = useState({ name: 'Ana', idade: 19 })
   return (
     <>
       <nav className={styles.navbar}>
-        <p>{user.name}</p>
-        <p>{user.idade}</p>
-        <button onClick={() => setUser((prevUser) => ({...prevUser, idade:prevUser.idade + 1}))}>Aumenta idade</button>
+        <ul className={styles.link_list}>
+          <li>Home</li>
+          <li>Register</li>
+          <li>New Post</li>
+          <li>Dasboard</li>
+          <li>About Us</li>
+          <li>
+            <button className={styles.logout}>Exit</button>
+          </li>
+        </ul>
       </nav>
     </>
   )
