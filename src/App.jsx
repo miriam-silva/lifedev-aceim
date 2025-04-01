bb53798d6127be500491f0825a487a36cc05992e
EloisaSilva016 <eloisasilvafatec@gmail.com>
2025-04-01 09:50:12 -0300

    Criação de pages no projeto lifedev-aceim

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home/home' //Se não for trocar o segundo "H" por maiusculo 
+import Home from './pages/Home/Home' //Se não for trocar o segundo "H" por maiusculo 
 
 
 function App() {
