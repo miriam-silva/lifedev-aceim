4a3935df2238a4c90dca465bd07392e4aa90d782
carolineolvs <carolineolvs0@gmail.com>
2025-05-18 23:27:26 -0300

    Parte 03 da aula do dia 13/05

--- src/hooks/useFetchDocument.jsx
+++ src/hooks/useFetchDocument.jsx
@@ -1,14 +1,31 @@
 import {useState, useEffect} from "react"
 import {db} from "../firebase/config"
-import {
-    collect, 
-    query, 
-    orderBy, 
-    onSnapshot, 
-    where,
+import {doc, getDoc} from "firebase/firestore"
 
-} from "firebase/firestore"
+export const useFetchDocument = (docCollection, id) => {    
+    const [document, setDocument] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
 
-export const useFetchDocument = () =>{
-    
+    useEffect(() => {
+        const loadDocument = async () => {
+            setLoading(true)
+            try {
+                const docRef = await doc(db, docCollection, id)
+                const docSnap = await getDoc(docRef)
+
+                setDocument(docSnap.data())
+            } catch (error) {
+                console.log(error)
+                setError(error.message)
+            }
+            setLoading(false)
+        }
+
+        loadDocument()
+    }, [docCollection, id])
+
+    console.log(document)
+
+    return {document, error, loading}
 }
--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -1,7 +1,69 @@
-import {useState, useEffect} from "react"
+import {useState, useEffect, use} from "react"
 import {db} from "../firebase/config"
-import {doc, deleteDoc} from "firebase/firestore"
+import {
+    collection,
+    orderBy,
+    query,
+    where,
+    onSnapshot
+} from "firebase/firestore"
 
-export const useFetchDocuments = () =>{
-    
-}
+export const useFetchDocuments = (docCollection, search = null, uid = null) =>{
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(() => {
+
+        async function loadData() {
+            if(cancelled) {
+                return
+            }
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+            try {
+                let q
+
+                if(search){
+                    q = await query(
+                        collectionRef,
+                        where("tags", "array-contains", search),
+                        orderBy("createdAt", "desc")
+                    )
+                } else if(uid){
+                    q = await query(
+                        collectionRef,
+                        where("uid", "==", uid),
+                        orderBy("createdAt", "desc")
+                    )
+                } else {
+                    q = await query(
+                        collectionRef,
+                        orderBy("createdAt", "desc")
+                    )
+                }
+
+                await onSnapshot(q, (querySnapshot) => {
+                    setDocuments(querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data(), })))
+                })
+
+            } catch (error) {
+                console.log(error)
+                setError(error.message)
+            }
+            setLoading(false)
+        }
+
+        loadData()
+    }, [docCollection, search, uid, cancelled])
+    console.log(documents)
+
+    useEffect(() => {
+        return () => 
+            setCancelled(true)
+        }, [])
+        return {documents, error, loading}
+    }
