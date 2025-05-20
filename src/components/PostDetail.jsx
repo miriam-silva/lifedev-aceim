cd52c39b3b34d0bc6614d22f9c1ff644aa9fdbea
isageorgete <isadorageorgete22@gmail.com>
2025-05-20 12:51:52 -0300

    Parte2-Aula 20/05

---
+++ src/components/PostDetail.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import styles from "./PostDetail.module.css"
+ 
+const PostDetail = ({post}) => {
+  return (
+    <>
+        <div className={styles.post_detail}>
+            <img src={post.image} alt={post.title} />
+            <h2>{post.title}</h2>
+            <p className={styles.createdby}>por: {post.createdby}</p>
+            <div className={styles.tags}>{post.tags.map((tags) => (
+                <p key={tag}>
+                    <span>#</span>{tag}
+                </p>))}
+            </div>
+            <p className={styles.createdby}>data: {post.createAt}</p>
+            <Link to={`/post/${post.id}`} className="btn btn-outline">Ler Postagem</Link>
+        </div>
+    </>
+  )
+}
+ 
+export default PostDetail
